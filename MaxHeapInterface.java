448783090361843fd0a1e802b79e6b36d944814b
Maya Tene <maya@Piglets-Tail.local>
2021-10-29 18:54:05 -0700

    Adds Interface and class files

---
+++ MaxHeapInterface.java
@@ -0,0 +1,10 @@
+package src.main.java;
+public interface MaxHeapInterface<T extends Comparable<? super T>>
+{
+    public void add(T newEntry);
+    public T removeMax();
+    public T getMax();
+    public boolean isEmpty();
+    public int getSize();
+    public void clear();
+}
