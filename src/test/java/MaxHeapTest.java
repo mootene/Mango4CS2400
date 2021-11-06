a093fcb6f29ac00c82239f4563b7aa128931ceaf
Maya Tene <maya@Piglets-Tail.local>
2021-11-06 15:56:31 -0700

    Adds new mvn test methods to MaxHeapTest.java

--- src/test/java/MaxHeapTest.java
+++ src/test/java/MaxHeapTest.java
@@ -2,8 +2,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
 public class MaxHeapTest
 {
+   @Test
+   void testConstructors()
+   {
+      MaxHeap<Integer> heap1 = new MaxHeap<>();
+      MaxHeap<Integer> heap2 = new MaxHeap<>(26);
+
+      assertEquals(0, heap1.getSize());
+      assertEquals(heap1.getSize(), heap2.getSize());
+   }
    public static void main(String args[])
    {
         Scanner inputScnr = new Scanner(System.in);
